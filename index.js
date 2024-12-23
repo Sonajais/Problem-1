71a3eec57c3cda681d93dbccf441a9c8091ac280
Sonam Jaiswal <141503130+Sonajais@users.noreply.github.com>
2024-12-23 17:58:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// Example1
 let name="Amolya Sharma";
 let count={};
 for (let i=0; i<name.length; i++){
@@ -13,4 +14,21 @@ else{
 for(let char in count){
   console.log($(char):$(count[char]}');
 }
-                
+
+// Example 2
+let name="Chinmay Kulkarni";
+let count={};
+for (let i=0; i<name.length; i++){
+  let(char=name[i].toLowerCase();
+  if(count[char]){
+                count[char]++;
+  }
+else{
+  count[char]=1;
+}
+}
+}
+for(let char in count){
+  console.log($(char):$(count[char]}');
+}
+    
