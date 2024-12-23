c53573227e00964aa7d7277431f4dd9bcd99178c
Sonam Jaiswal <141503130+Sonajais@users.noreply.github.com>
2024-12-23 17:56:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 let name="Amolya Sharma";
-let count=0;
-for (let i=0; i<name.length,i++)(
+let count={};
+for (let i=0; i<name.length; i++){
   let(char=name[i].toLowerCase();
-  if(char.match(/[a-z]){
-                if(count[char]){
+  if(count[char]){
                 count[char]++;
   }
 else{
@@ -12,6 +11,6 @@ else{
 }
 }
 for(let char in count){
-  console.log('$(char):$(count[char]}');
+  console.log($(char):$(count[char]}');
 }
                 
